e3dc251aeac32b22e72158b0e84883ffe514e8a8
1da1ana1 <yasmimdaiana10@gmail.com>
2025-10-22 22:56:55 -0300

    criado o header e o estilo básico

--- frontend/src/components/HeaderMenu.vue
+++ frontend/src/components/HeaderMenu.vue
@@ -1,9 +1,8 @@
 <template>
-<header>
-    <img src="/src/assets/img/logos-header.png" alt="logo ft" class="header">
-
-    <nav class="header__nav">
-      <ul class="header__nav-list">
+  <header class="header">
+    <img src="/src/assets/img/logos-header.png" alt="logo ft">
+    <nav class="header-nav">
+      <ul class="header-links">
         <li><a href="#" class="header__nav-item">PÁGINA INICIAL</a></li>
         <li><a href="#" class="header__nav-item">BUSCAR ORIENTADOR</a></li>
         <li><a href="#" class="header__nav-item">MEU PERFIL</a></li>
@@ -12,48 +11,18 @@
   </header>
 </template>
 
-<style scoped>
+<style>
 .header {
   display: flex;
+  align-items: center;
   justify-content: space-between;
-  align-items: center;
-  background: linear-gradient(90deg, var(--ft-color-blue), var(--ft-color-green));
-  padding: 0.8rem 2rem;
-  color: var(--text-color-secondary);
+  padding: 0 20px;
+  background: linear-gradient(90deg, var(--color-brand-primary), var(--color-brand-secondary));
+  height: 80px;
+};
+
+.header-links li{
+    text-decoration: none
 }
 
-.header__logo {
-  display: flex;
-  align-items: center;
-  gap: 1rem;
-}
-
-.header__logo-img {
-  height: 40px;
-}
-
-.header__ft-logo {
-  height: 36px;
-}
-
-.header__nav-list {
-  display: flex;
-  gap: 1rem;
-  list-style: none;
-}
-
-.header__nav-item {
-  text-decoration: none;
-  color: var(--text-color-secondary);
-  background-color: var(--ft-color-blue);
-  padding: 0.6rem 1.2rem;
-  border-radius: 2rem;
-  transition: background-color 0.3s;
-  font-size: 0.9rem;
-  font-weight: 500;
-}
-
-.header__nav-item:hover {
-  background-color: var(--text-color-submenu-hover);
-}
 </style>
