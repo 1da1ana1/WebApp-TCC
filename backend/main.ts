d2ba8dbc8cb1c3d3629c1596f50604b56ed98154
Leo <leo.sato957@gmail.com>
2025-11-18 23:07:42 -0300

    fix: removed duplicated app directory

--- backend/main.ts
+++ backend/main.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from 'app/routes/app.module';
+import { AppModule } from './src/app/routes/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
