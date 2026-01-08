f1eebaa3ef24e4c1e3c4762f026ac4326187c9c8
1da1ana1 <yasmimdaiana10@gmail.com>
2026-01-08 19:20:27 -0300

    estrutura da barra de pesquisa pronta

--- frontend/src/components/SearchBar.vue
+++ frontend/src/components/SearchBar.vue
@@ -1,1 +1,17 @@
-<template></template>
+<template>
+  <div class="search-bar-container">
+    <input 
+      type="text" 
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+      placeholder="ex: João da Silva" 
+      class="search-input"
+    />
+    <i class="bi bi-search search-icon"></i>
+  </div>
+</template>
+
+<script setup>
+defineProps(['modelValue']);
+defineEmits(['update:modelValue']);
+</script>
