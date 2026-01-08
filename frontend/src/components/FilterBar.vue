267708c09257da01390f2ae3a5c1f0977a269c3b
1da1ana1 <yasmimdaiana10@gmail.com>
2026-01-08 19:21:27 -0300

    estrutura do filtro lateral criada

--- frontend/src/components/FilterBar.vue
+++ frontend/src/components/FilterBar.vue
@@ -1,5 +1,33 @@
-<template></template>
+<template>
+  <aside class="filter-sidebar">
+    <h3>Filtrar professor por temas</h3>
+    
+    <div class="checkbox-group">
+      <label v-for="theme in themes" :key="theme" class="checkbox-item">
+        <input 
+          type="checkbox" 
+          :value="theme" 
+          @change="toggleTheme(theme)"
+        />
+        <span>{{ theme }}</span>
+      </label>
+    </div>
 
-<style>
+    <button class="btn-clear" @click="$emit('clear')">Limpar seleção</button>
+  </aside>
+</template>
 
-</style>
+<script setup>
+const props = defineProps(['themes', 'selectedThemes']);
+const emit = defineEmits(['update:selectedThemes', 'clear']);
+
+const toggleTheme = (theme) => {
+  const newSelection = [...props.selectedThemes];
+  const index = newSelection.indexOf(theme);
+  
+  if (index > -1) newSelection.splice(index, 1);
+  else newSelection.push(theme);
+  
+  emit('update:selectedThemes', newSelection);
+};
+</script>
